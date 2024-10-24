d9573561a6e0c4ad2c511040e3c65dd5759a34fb
Saini <85294275+Upiottxat@users.noreply.github.com>
2024-10-24 17:23:50 +0530

    add JWT

--- Controller/auth.controller.js
+++ Controller/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs"
 import dotenv from 'dotenv';
 import { CreateNew, GetUser } from '../Model/User.model.js';
+import { GenerateToken } from "../utils/jwt.js";
 dotenv.config()
 export const login = async (req, res) => {
 
@@ -12,7 +13,10 @@ export const login = async (req, res) => {
         if (user) {
             const passwordCheck = await bcrypt.compare(Password, user.Password)
             // console.log(passwordCheck);
-            if (passwordCheck) return res.status(200).json({ msg: "login success" })
+            if (passwordCheck) {
+                await GenerateToken(user.userId, res)
+                return res.status(200).json({ msg: "login success" })
+            }
 
         } else {
             return res.status(404).json({ error: "Invalid Email or password" })
@@ -34,6 +38,7 @@ export const signup = async (req, res) => {
         const salt = await bcrypt.genSalt(rounds)
         const HashPassword = await bcrypt.hash(Password, salt)
         const user = await CreateNew(Name, Email, HashPassword)
+        if (!user.error) await GenerateToken(user.userId, res)
         return res.status(200).json(user)
     } catch (error) {
         console.log(error);
